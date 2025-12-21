e76c5009318cbe7020f4bdec8f0d1a8683046787
Naxrul Ahmed <devnaxrul@gmail.com>
2025-12-21 09:03:52 +0600

    reformated the useTagProps

--- src/Tag/useTagProps.ts
+++ src/Tag/useTagProps.ts
@@ -3,41 +3,59 @@ import cssPropList from './cssPropList';
 import { css } from '../css';
 import { classNames } from 'pretty-class';
 import { CSSFactoryType } from 'oncss';
+import { useMemo } from 'react';
 
 export type useTagPropsReturn<T extends TagComponentType = "div"> = {
    props: TagProps<T>,
    style: CSSFactoryType
 }
 
-const useTagProps = <T extends TagComponentType = "div">({ baseClass, classNames: clses, sx, sxr, style, hover, ...props }: TagPropsRoot<T>): useTagPropsReturn<T> => {
-   const _css: any = {
-      ...(sxr || {}),
-      ...props,
-      ...(sx || {}),
-      ...(style || {}),
-   }
+const useTagProps = <T extends TagComponentType = "div">(props: TagPropsRoot<T>): useTagPropsReturn<T> => {
 
-   if (hover) {
-      _css['&:hover'] = { ...(_css['&:hover'] || {}), ...hover }
-   }
+   const parsed = useMemo(() => {
+      let _props: any = {}
+      let _css: any = {}
+      let clss = []
+      for (let key in props) {
+         let val = (props as any)[key];
+         if (key === "className") {
+            clss.push(val)
+         } else if (key === 'baseClass') {
+            clss.push("xui-" + val)
+         } else if (key === 'classNames') {
+            clss.push(...val)
+         } else if (key === "sx" || key === "sxr" || key === "style") {
+            _css = {
+               ..._css,
+               ...val
+            }
+         } else if (key === "hover") {
+            _css['&:hover'] = { ...(_css['&:hover'] || {}), ...val }
+         } else if (!cssPropList[key]) {
+            _props[key] = val
+         } else {
+            _css[key] = val
+         }
+      }
+      const styles = css(_css, {
+         injectStyle: typeof window !== 'undefined',
+      })
 
-   const styles = css(_css, {
-      skipProps: (prop, _val, dept): any => dept === 1 && !cssPropList[prop],
-      injectStyle: typeof window !== 'undefined',
-   })
+      clss.push(styles.classname)
+      return {
+         props: _props,
+         styles,
+         className: classNames(clss)
+      }
+   }, [JSON.stringify(props)])
 
-   let skipProps = styles.skiped[styles.classname as any] || []
    const _props: any = {};
-   for (let prop of skipProps) {
+   for (let prop in parsed.props) {
       _props[prop] = (props as any)[prop]
    }
-   _props.className = classNames([
-      baseClass ? "xui-" + baseClass : "",
-      styles.classname,
-      props.className,
-      ...(clses as any || []),
-   ])
-   return { props: _props, style: styles };
+   _props.className = parsed.className;
+
+   return { props: _props, style: parsed.styles };
 }
 
 
